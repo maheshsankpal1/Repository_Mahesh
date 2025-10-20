1366913e60b37810dae515a4c8c6b047a95a79e4
Mahesh Sankpal <maheshsankpal59@gmail.com>
2025-10-20 14:02:22 +0530

    second commit

--- src/test/java/arttoframe/Crm_Email/Testbillionaire.java
+++ src/test/java/arttoframe/Crm_Email/Testbillionaire.java
@@ -1,5 +1,6 @@
 package arttoframe.Crm_Email;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import arttoframe.Crm_Email_PageObject.billionaire;
@@ -7,34 +8,47 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Testbillionaire {
-	
-	  @Test
-	    public void openbillionaire() {
-	        // Setup ChromeDriver automatically
-	        WebDriverManager.chromedriver().setup();
-	        WebDriver driver = new ChromeDriver();
-	       
 
-	        // Maximize window
-	        driver.manage().window().maximize();
+	@Parameters("Browser")
+	@Test
+	public void openbillionaire(String browserName) {
+		System.out.println("Parameter value is" +browserName);
+		WebDriver driver = null;
+		
+		if (browserName.contains("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if (browserName.contains("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+			
+		// Setup ChromeDriver automatically
+		
 
-	        // Open URL using billionaire class
-	        billionaire page = new billionaire(driver);
-	        page.openUrl("https://www.billionaire.com/in/home");
+		// Maximize window
+		driver.manage().window().maximize();
 
-	        // Optional: wait a few seconds to see the browser
-	        try {
-	            Thread.sleep(3000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
+		// Open URL using billionaire class
+		billionaire page = new billionaire(driver);
+		page.openUrl("https://www.billionaire.com/in/home");
 
-	        // Close browser
-	        driver.quit();
-	    }
+		// Optional: wait a few seconds to see the browser
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Close browser
+		driver.quit();
 	}
-	
-
-
+}
